8a0ed470acb2a9979920e18641376178e8b82362
varsha-wagh-45 <varshawagh450@gmail.com>
2025-08-16 19:57:00 +0530

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,16 +2,26 @@ import { useState } from 'react'
 import './assets/css/style.css'
 import Header from './componants/Header'
 import Main from './componants/main'
-import Footer from './componants/footer'
+import Footer from './componants/Footer'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Register from './componants/Register'
+import Login from './componants/Login'
 
 function App() {
   
 
   return (
+    
     <>
-      <Header/>
-      <Main />
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login/>}/>
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- frontend-react/src/componants/Main.jsx
+++ frontend-react/src/componants/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Button from './Button'
+import Header from './Header'
+import Footer from './Footer'
+
 
 const Main = () => {
   return (
     <>
+      
       <div className='container'>
         <div className='p-5 text-center bg-light-dark rounded'>
           <h1 className='text-light'>Stock Predicion Portal</h1>
@@ -11,7 +15,8 @@ const Main = () => {
           <Button text="Login" class='btn btn-outline-info'/>
           
         </div>
-       </div>
+      </div>
+     
     </>
   )
 }
